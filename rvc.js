480af679eaf0ea4aea649be6ec369e5353483cde
Sankera <Sankeragaming@gmail.com>
2017-10-30 12:17:43 +0100

    Code now creates 2 images of the command and the object
    H

--- rvc.js
+++ rvc.js
@@ -3,7 +3,7 @@ const chroma = require('chroma-js');
 
 var ttD = {};
 
-jimp.read('./img/test_mena.png', function(err, img) {
+jimp.read('./img/test_band.png', function(err, img) {
 	if (err) {
 		console.log(err);
 		return;
@@ -57,8 +57,35 @@ jimp.read('./img/test_mena.png', function(err, img) {
 		console.log('We have enough info to extract the tooltip!!!');
 		let tooltip = img.clone();
 		tooltip.crop(ttD.tlX, ttD.tlY, (ttD.brX - ttD.tlX), (ttD.brY - ttD.tlY))
-			.scale(2)
-			.write(`./img/tooltip.${img.getExtension()}`);
+			.scale(2);
+			let objecttooltip = tooltip.clone();
+			let commandtooltip = tooltip.clone();
+			
+			console.log('Looking for the object...');
+			objectloop:
+			for (let i = 0; i < objecttooltip.bitmap.height; i++) {
+				for (let j = 0; j < objecttooltip.bitmap.width; j++) {
+					let color = objecttooltip.getPixelColor(j, i);
+					if (color == 0x00d9d9FF) {
+						objecttooltip.crop(j - 10 , 0, objecttooltip.bitmap.width - (j - 10), objecttooltip.bitmap.height)
+						.write(`./img/tooltipobject.${img.getExtension()}`);
+						break objectloop;
+					}
+				}
+			}
+
+			console.log('Looking for the command...');
+			commandloop:
+			for (let i = 0; i < commandtooltip.bitmap.height; i++) {
+				for (let j = 0; j < commandtooltip.bitmap.width; j++) {
+					let color = commandtooltip.getPixelColor(j, i);
+					if (color == 0x00d9d9FF) {
+						commandtooltip.crop(0 , 0, j - 10, commandtooltip.bitmap.height)
+						.write(`./img/tooltipcommand.${img.getExtension()}`);
+						break commandloop;
+					}
+				}
+			}
 	} else {
 		console.log('Failed to find some or all elements. Sorry!');
 	}
